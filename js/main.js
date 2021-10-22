fb4942e56aecae463ab629453180aed90fe8afc1
s1rserg <87946455+s1rserg@users.noreply.github.com>
2021-10-22 20:34:33 +0300

    copy buttons were added
    :)

--- js/main.js
+++ js/main.js
@@ -1,22 +1,45 @@
 const url = "https://api.shrtco.de/v2/shorten?url=";
-var formInput = document.getElementById("form__input");
+let formInput = document.getElementById("form-input");
+let formButton = document.getElementById("form-button");
+let outputLink1 = document.getElementById("shortened-link1");
+let outputLink2 = document.getElementById("shortened-link2");
+let outputLink3 = document.getElementById("shortened-link3");
+let links1 = document.getElementById("shortened-links1")
+let links2 = document.getElementById("shortened-links2")
+let links3 = document.getElementById("shortened-links3")
 
-var el = document.getElementById("outside");
-var el1 = document.getElementById("inside");
-var el2 = document.getElementById("inside2");
-var el3 = document.getElementById("inside3");
-el.addEventListener("click", myFunction , false);
 
-function myFunction() {
-    var link = url + formInput.value;
+
+formButton.addEventListener("click", shortenLink, false);
+
+links1.style.display = "none";
+links2.style.display = "none";
+links3.style.display = "none";
+
+
+function shortenLink() {
+    let link = url + formInput.value;
 
     fetch(link)
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            el1.innerHTML = data.result.short_link;
-            el2.innerHTML = data.result.short_link2;
-            el3.innerHTML = data.result.short_link3;
+            links1.style.display = "flex";
+            links2.style.display = "flex";
+            links3.style.display = "flex";
+            outputLink1.innerHTML = data.result.short_link;
+            outputLink2.innerHTML = data.result.short_link2;
+            outputLink3.innerHTML = data.result.short_link3;
         });
+}
+
+function copy(elementId) {
+  var aux = document.createElement("input");
+
+  aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+  document.body.appendChild(aux);
+  aux.select();
+  document.execCommand("copy");
+  document.body.removeChild(aux);
 }
