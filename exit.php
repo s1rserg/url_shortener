fd249e3b8b6f7a384c4e4490c663e510bde0a468
s1rserg <87946455+s1rserg@users.noreply.github.com>
2021-11-01 19:59:17 +0200

    Delete exit.php

--- exit.php
+++
@@ -1,6 +0,0 @@
-<?php
-    setcookie('user', $user['name'], time() - 3600, "/");
-    setcookie('id', $user['id'], time() - 3600, "/");
-    setcookie('link1', $user['link1'], time() - 3600, "/");
-    header('Location: /');
-?>
